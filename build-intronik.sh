712d8cb563f7a61024a7aef910c2f50f16f07f58
Daniel Gross <dgross@intronik.de>
2016-10-24 17:11:08 +0200

    cancom2 adc channel 0&1

--- build-intronik.sh
+++ build-intronik.sh
@@ -3,17 +3,17 @@
 DIR=$PWD
 echo building intronik kernel for beagle bone
 export CC=/home/INTRONIK/linuxdev/gcc-4.9/bin/arm-linux-gnueabihf-
-export CORES=8
-export BUILD=intronik5
+export CORES=1
+export BUILD=intronik7
 export DISTRO=cross
 export DEBARCH=armhf
 export ARCH=arm
-# export config="intronik_defconfig"
+export config="intronik_defconfig"
 export DEPLOY=/home/INTRONIK/linuxdev/deploy
 export LOCALVERSION=-${BUILD}
 export CROSS_COMPILE="${CC}"
 mkdir -p ${DEPLOY}
 # make --jobs=${CORES} menuconfig
 # make --jobs=${CORES} zImage modules dtbs
-# fakeroot make -j${CORES} ARCH=arm KBUILD_DEBARCH=${DEBARCH} LOCALVERSION=-${BUILD} CROSS_COMPILE=${CC} KDEB_PKGVERSION=1${DISTRO} deb-pkg
+fakeroot make -j${CORES} ARCH=arm KBUILD_DEBARCH=${DEBARCH} LOCALVERSION=-${BUILD} CROSS_COMPILE=${CC} KDEB_PKGVERSION=1${DISTRO} deb-pkg
 
