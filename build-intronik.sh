856909106a4904a0cb509432f8fd1a877450b8d4
Intronik GmbH <kontakt@intronik.de>
2020-01-14 13:46:44 +0100

    4.1.3-intronik9 DG
    - debian package build
    - update THERMONOM DTB build

--- build-intronik.sh
+++ build-intronik.sh
@@ -2,18 +2,17 @@
 # settings start here
 DIR=$PWD
 echo building intronik kernel for beagle bone
-export CC=/home/INTRONIK/linuxdev/gcc-4.9/bin/arm-linux-gnueabihf-
+export CC=/home/intronik/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 export CORES=1
-export BUILD=intronik7
+export BUILD=intronik9
 export DISTRO=cross
 export DEBARCH=armhf
 export ARCH=arm
 export config="intronik_defconfig"
-export DEPLOY=/home/INTRONIK/linuxdev/deploy
+export DEPLOY=/home/intronik/deploy
 export LOCALVERSION=-${BUILD}
 export CROSS_COMPILE="${CC}"
 mkdir -p ${DEPLOY}
 # make --jobs=${CORES} menuconfig
 # make --jobs=${CORES} zImage modules dtbs
 fakeroot make -j${CORES} ARCH=arm KBUILD_DEBARCH=${DEBARCH} LOCALVERSION=-${BUILD} CROSS_COMPILE=${CC} KDEB_PKGVERSION=1${DISTRO} deb-pkg
-
